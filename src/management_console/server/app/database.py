936b9885ad483f554894ff191a2011661eb25f03
Diep Gia Huy <Huycan1211@gmail.com>
2026-05-09 22:33:40 +0700

    fix: correct typo in foreign key naming convention for PostgreSQL

--- src/management_console/server/app/database.py
+++ src/management_console/server/app/database.py
@@ -19,7 +19,7 @@ POSTGRES_NAMING_CONVENTION = {
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referenced_table_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 }
 class Base(DeclarativeBase):
