2e767a26d356a33ab93a720640462041e6fba33f
Diep Gia Huy <Huycan1211@gmail.com>
2026-05-09 22:16:38 +0700

    feat: add PostgreSQL naming convention to avoid Alembic autogenerate issues

--- src/management_console/server/app/database.py
+++ src/management_console/server/app/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
 from app.config import get_settings
 from typing import AsyncGenerator
@@ -13,9 +14,16 @@ async_session_maker = async_sessionmaker(
     expire_on_commit=False
 )
 
-
+# add naming convention for PostgreSQL to avoid issues with Alembic autogenerate
+POSTGRES_NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referenced_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention=POSTGRES_NAMING_CONVENTION)
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
