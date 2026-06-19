ac1b2bc7e5cc1f6d3af815470b320ee3a36cd8ce
Diep Gia Huy <Huycan1211@gmail.com>
2026-06-19 22:35:33 +0700

    refactor: remove AGENT_SECRET_KEY

--- src/management_console/server/app/config.py
+++ src/management_console/server/app/config.py
@@ -28,7 +28,6 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 480  # 8 hours
-    AGENT_SECRET_KEY: str
     
     # server settings
     HEARTBEAT_INTERVAL_SECONDS: int = 60
